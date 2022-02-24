d44bd2b7df0bfdda3fc7c432305b2d14c0cfd0ab
Andrew Yurick <andrewy2015a@gmail.com>
2022-02-24 14:53:40 -0500

    Begin using JS.

---
+++ index.js
@@ -0,0 +1,9 @@
+//This is my index.js File
+//First importing expresss and morgan
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();//First call to express to initialize
+
+app.use(morgan("dev"));
+const port = 8080; //I hate UDP and TCP thanks to ee/cmpen 362
